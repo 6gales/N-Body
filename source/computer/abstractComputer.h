bb070acf6e3c69fd5b86484be5b1539bfce26b40
YGAR84 <a.lyamin@g.nsu.ru>
2019-07-02 14:09:09 +0700

    default ctors

--- source/computer/abstractComputer.h
+++ source/computer/abstractComputer.h
@@ -2,23 +2,23 @@
 
 struct Particle
 {
-	double mass,
-			x,
-			y,
-			z;
+	double mass = 0.0,
+			x = 0.0,
+			y = 0.0,
+			z = 0.0;
 
 	Particle(double _mass, double _x, double _y, double _z)
 			: mass(_mass), x(_x), y(_y), z(_z) {}
-
-
+	Particle(){}
 };
 
 struct WideParticle : public Particle
 {
-	double v_x,
-			v_y,
-			v_z;
+	double v_x = 0.0,
+			v_y = 0.0,
+			v_z = 0.0;
 
+	WideParticle() : Particle() {}
 	WideParticle(double _mass, double _x, double _y, double _z,
 				 double _v_x, double _v_y, double _v_z)
 			: Particle(_mass, _x, _y, _z), v_x(_v_x), v_y(_v_y), v_z(_v_z) {}
