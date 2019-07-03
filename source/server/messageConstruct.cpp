c2325a5a75124dc4e9a7e0e94a0a21f5fbc34bd3
maksimych <123456vershinin@gmail.com>
2019-07-03 12:57:14 +0700

    few changes

--- source/server/messageConstruct.cpp
+++ source/server/messageConstruct.cpp
@@ -8,14 +8,9 @@
 std::string construct_data_message(const Particle* particles, size_t count) {
     std::string send_msg{};
     char counts[8];
-    counts[0] = (char)(count >> 56);
-    counts[1] = (char)(count >> 48);
-    counts[2] = (char)(count >> 40);
-    counts[3] = (char)(count >> 32);
-    counts[4] = (char)(count >> 24);
-    counts[5] = (char)(count >> 16);
-    counts[6] = (char)(count >> 8);
-    counts[7] = (char)(count);
+    for (size_t j = 0; j < 8; ++j) {
+        counts[j] = (char)(count >> ((7-j)*8));
+    }
     send_msg.append(counts);
     for (size_t i = 0; i < count; ++i) {
         float mass = particles[0].getMass();
