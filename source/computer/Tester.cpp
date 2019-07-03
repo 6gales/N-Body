12d605946c007706d2948e4307d4ab1d3db0c108
YGAR84 <a.lyamin@g.nsu.ru>
2019-07-03 11:38:00 +0700

    add tester

---
+++ source/computer/Tester.cpp
@@ -0,0 +1,43 @@
+#include "computer.h"
+#include <fstream>
+#include <memory>
+#include <iostream>
+
+
+int main()
+{
+	std::ifstream in("in.txt");
+	std::ofstream out("out.txt");
+
+	Computer* computer = getInstanceOf();
+	int N;
+	in >> N;
+
+	std::shared_ptr<Particle> parts(new Particle[N]);
+
+
+	for(int i = 0; i < N; ++i)
+	{
+		double mass, x, y, z, vx, vy, vz;
+		in >> mass >> x >> y >> z >> vx >> vy >> vz;
+		parts.get()[i] = Particle(mass, x, y, z, vx, vy, vz);
+		std::cout << parts.get()[i] << std::endl;
+	}
+	in.close();
+
+	computer->init(parts, N);
+
+	for(int iter = 0; iter < 10; ++iter)
+	{
+		out << "====================ITERATION "<< iter <<"====================="<< std::endl;
+		Particle* buff = computer->iterate();
+		for(int i = 0; i < N; ++i)
+		{
+			std::cout << buff[i] << std::endl;
+			out << buff[i] << std::endl;
+		}
+		out << "====================ITERATION "<< iter <<"====================="<< std::endl << std::endl;
+	}
+
+	out.close();
+}
--- source/computer/Vector3D.cpp
+++ source/computer/Vector3D.cpp
@@ -19,3 +19,10 @@ Vector3D operator-(const Vector3D &v1, const Vector3D &v2)
 {
 	return Vector3D(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
 }
+
+
+std::ostream& operator<<(std::ostream &os, const Vector3D &v)
+{
+	os << "(" << v.x << "," << v.y << "," << v.z << ")";
+	return os;
+}
