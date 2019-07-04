1716a56abbeae3d75daccffaf067daf40d9e9e53
6gales <6gales@gmail.com>
2019-07-04 12:52:18 +0700

    fix bodies collision

--- source/computer/ompComputer.h
+++ source/computer/ompComputer.h
@@ -11,6 +11,11 @@ private:
 	void fillForces();
 
 public:
+	ompComputer(size_t numThreads)
+	{
+		omp_set_num_threads(numThreads);
+		forces = nullptr;
+	}
 	void init(std::shared_ptr<Particle>, size_t);
 	Particle* iterate();
 	~ompComputer();
