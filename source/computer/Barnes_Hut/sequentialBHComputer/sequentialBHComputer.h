e7fb69b16d542356f3e5ae380e92631200fc79f1
YGAR84 <a.lyamin@g.nsu.ru>
2019-07-11 18:02:01 +0700

    add Tester && change formula for force in Barnes-Hut

--- source/computer/Barnes_Hut/sequentialBHComputer/sequentialBHComputer.h
+++ source/computer/Barnes_Hut/sequentialBHComputer/sequentialBHComputer.h
@@ -8,7 +8,6 @@ class SequentialBHComputer : public Computer
 {
 private:
 	const double theta = 0.1;
-
 	Vector3D* Acces = nullptr;
 	Task *t;
 
---
+++ source/computer/Tester/Tester.cpp
@@ -0,0 +1,13 @@
+#include <fstream>
+#include <iostream>
+#include "../computer.h"
+
+
+
+int main(int argc, char* argv[])
+{
+	std::ifstream in("in.txt");
+
+
+
+}
