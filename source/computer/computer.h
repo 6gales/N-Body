635055dbffa490b0e21f9bd04885de110af584c5
maksimych <123456vershinin@gmail.com>
2019-07-11 18:45:23 +0700

    added ompBH, new struct directories

--- source/computer/computer.h
+++ source/computer/computer.h
@@ -2,12 +2,12 @@
 
 #include <memory>
 #include "abstractComputer.h"
-#include "sequentialComputer/sequentialComputer.h"
+#include "./N_square/sequentialComputer/sequentialComputer.h"
 #include "Runge_Kuta/sequentialRKComputer/sequentialRKComputer.h"
-#include "sequentialBHComputer/sequentialBHComputer.h"
-#include "ompComputer/ompComputer.h"
+#include "./Barnes_Hut/sequentialBHComputer/sequentialBHComputer.h"
+#include "./N_square/ompComputer/ompComputer.h"
 #include "Runge_Kuta/ompRKComputer/ompRKComputer.h"
-//#include "mpiComputer/mpiComputer.h"
+//#include "./N_square/mpiComputer/mpiComputer.h"
 #include "ComputerType.h"
 
 std::shared_ptr<Computer> getInstanceOf(ComputerType type, size_t threads);
